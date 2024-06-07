b258171d8ea49a98d1a592f1991cdd12a9fb7065
AishaKeita <aishacassel@gmail.com>
2024-06-07 12:33:02 +0200

    Managing event

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,18 @@
+import React, { useState } from 'react'
+
 function Home() {
+    const [count, setCount] = useState(0)
+
+    function HandleClick() {
+        setCount(count + 1)
+    }
+
     return (
         <>
             <div>My lab1 assignment</div>
-            <button>Submit</button>
+            <button onClick={HandleClick}>Count</button>
+            <br />
+            <div>{count}</div>
         </>
     )
 }
