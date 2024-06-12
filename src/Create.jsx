22ac2a25e18491574793d3ceaba3aed5db961388
AishaKeita <aishacassel@gmail.com>
2024-06-12 10:53:11 +0200

    Add useRef

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,26 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
 function Create() {
-  const [post, setPost] = useState([]);
+    const [post, setPost] = useState([])
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/posts/1")
-      .then((res) => res.json())
-      .then((post) => setPost(post, JSON.stringify(post)));
-  }, [post]);
+    useEffect(() => {
+        fetch('https://dummyjson.com/posts/1')
+            .then((res) => res.json())
+            .then((post) => setPost(post, JSON.stringify(post)))
+    }, [])
 
-  return (
-    <>
-      <div>
-        <div>Title: {post.title}</div>
-        <div>Body: {post.body}</div>
-        <div>Views: {post.views}</div>
-        {/* <div>Likes: {post.reactions.likes}</div>
-                <div>Likes: {post.reactions.dislikes}</div> */}
-        <div>Tags: {post.tags}</div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <div>Title: {post.title}</div>
+                <div>Body: {post.body}</div>
+                <div>Views: {post.views}</div>
+                <div>Tags: {post.tags}</div>
+            </div>
+        </>
+    )
 }
 
-export default Create;
+export default Create
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,112 +1,114 @@
-import React from "react";
-import { useState } from "react";
-import Create from "./Create";
-import UserDetails from "./userDetails";
-import ErrorBoundary from "./ErrorBoundary";
+import React from 'react'
+import { useState, useRef } from 'react'
+import Create from './Create'
+import UserDetails from './userDetails'
+import ErrorBoundary from './ErrorBoundary'
 
 function Form() {
-  const [showUserInfo, setUserInfo] = useState(false);
+    const [showUserInfo, setUserInfo] = useState(false)
+    const myButtonRef = useRef(null)
 
-  //state variable for input fields: name, lastname, age, city, email, and occupation
-  const [user, setUser] = useState({
-    name: "",
-    lastName: "",
-    age: "",
-    city: "",
-    email: "",
-    occupation: "",
-  });
+    //state variable for input fields: name, lastname, age, city, email, and occupation
+    const [user, setUser] = useState({
+        name: '',
+        lastName: '',
+        age: '',
+        city: '',
+        email: '',
+        occupation: ''
+    })
 
-  //function to handle submit change
-  //create a variable to assign the previous state
-  const submitChange = (event) => {
-    const { name, value } = event.target;
-    setUser((prevState) => ({ ...prevState, [name]: value }));
-    setUserInfo(true);
-  };
+    //function to handle submit change
+    //create a variable to assign the previous state
+    const submitChange = (event) => {
+        const { name, value } = event.target
+        setUser((prevState) => ({ ...prevState, [name]: value }))
+        setUserInfo(true)
+        myButtonRef.current.style.background = 'blue'
+    }
 
-  return (
-    <>
-      <div>
-        <ErrorBoundary>
-          <Create />
-        </ErrorBoundary>
-        <br />
-        <form>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={user.name}
-              onChange={submitChange}
-            />
-          </label>
-          <label>
-            Last name:
-            <input
-              type="text"
-              name="lastName"
-              value={user.lastName}
-              onChange={submitChange}
-            />
-          </label>
-          <label>
-            Age:
-            <input
-              type="text"
-              name="age"
-              value={user.age}
-              onChange={submitChange}
-            />
-          </label>
-          <label>
-            City:
-            <input
-              type="text"
-              name="city"
-              value={user.city}
-              onChange={submitChange}
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              type="text"
-              name="email"
-              value={user.email}
-              onChange={submitChange}
-            />
-          </label>
-          <label>
-            Occupation:
-            <input
-              type="text"
-              name="occupation"
-              value={user.occupation}
-              onChange={submitChange}
-            />
-          </label>
-        </form>
-        <br />
-        <button type="submit" onClick={submitChange}>
-          Submit
-        </button>
-
-        <br />
+    return (
         <>
-          <UserDetails
-            showUserInfo={showUserInfo}
-            name={user.name}
-            lastName={user.lastName}
-            age={user.age}
-            city={user.city}
-            email={user.email}
-            occupation={user.occupation}
-          />
+            <div>
+                <ErrorBoundary>
+                    <Create />
+                </ErrorBoundary>
+                <br />
+                <form>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            name="name"
+                            value={user.name}
+                            onChange={submitChange}
+                        />
+                    </label>
+                    <label>
+                        Last name:
+                        <input
+                            type="text"
+                            name="lastName"
+                            value={user.lastName}
+                            onChange={submitChange}
+                        />
+                    </label>
+                    <label>
+                        Age:
+                        <input
+                            type="text"
+                            name="age"
+                            value={user.age}
+                            onChange={submitChange}
+                        />
+                    </label>
+                    <label>
+                        City:
+                        <input
+                            type="text"
+                            name="city"
+                            value={user.city}
+                            onChange={submitChange}
+                        />
+                    </label>
+                    <label>
+                        Email:
+                        <input
+                            type="text"
+                            name="email"
+                            value={user.email}
+                            onChange={submitChange}
+                        />
+                    </label>
+                    <label>
+                        Occupation:
+                        <input
+                            type="text"
+                            name="occupation"
+                            value={user.occupation}
+                            onChange={submitChange}
+                        />
+                    </label>
+                </form>
+                <br />
+                <button ref={myButtonRef} type="submit" onClick={submitChange}>
+                    Submit
+                </button>
+
+                <br />
+                <>
+                    <UserDetails
+                        showUserInfo={showUserInfo}
+                        name={user.name}
+                        lastName={user.lastName}
+                        age={user.age}
+                        city={user.city}
+                        email={user.email}
+                        occupation={user.occupation}
+                    />
+                </>
+            </div>
         </>
-      </div>
-    </>
-  );
+    )
 }
-export default Form;
+export default Form
